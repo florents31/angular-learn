006e33f705974c9f4b967614fdde6a1482ffa36f
florent <florent.sampaio@me.com>
2018-08-23 10:45:38 +0200

    angular learning page service

---
+++ ng-demos/src/app/mood/mood.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoodService {
+
+
+  mood ='happy';
+
+
+
+  constructor() { }
+
+  updateMood(mood){
+
+    this.mood = mood;
+
+    console.log('changed to', mood);
+  }
+
+}
